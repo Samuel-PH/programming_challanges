64746c7c6f729ee8d661a7b0564a819e82090ca2
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 22:49:10 +0800

    Custom Title Function (DONE)

--- batch_6/batch_6_custom_title.py
+++ batch_6/batch_6_custom_title.py
@@ -1,2 +1,22 @@
 #title() makes all first letter of each word in the string, capital letter. And all other letter in small case. 
 #Create a program that do the same functionality without using title() function.
+
+def manual_title_case():
+    user_input = input("Please enter your text: ")
+    title_case_text = ""
+    is_new_word = True
+
+    for char in user_input:
+        if is_new_word:
+            title_case_text += char.upper()
+            is_new_word = False
+
+        else:
+            title_case_text += char.lower()
+
+        if char == " ":
+            is_new_word = True
+            
+    print(f"Result: {title_case_text}")
+
+manual_title_case()

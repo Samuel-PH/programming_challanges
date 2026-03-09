8673489a3d31d407f361fca81ebd8f8f952a73aa
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 15:39:21 +0800

    batch_1_print_all_even_numbers_0_to_100 is done

---
+++ batch_1/batch_1_print_all_even_numbers_0_to_100.py
@@ -0,0 +1,7 @@
+#Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+def print_even_numbers():
+    for current_number in range(0, 101, 2):
+        print(current_number)
+
+print_even_numbers()

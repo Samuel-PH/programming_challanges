4a6ab185eadd73a8ff5716cbbc45d7857afd4478
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 19:39:46 +0800

    batch_5_snake_case_converter (DONE) end of Batch 5

--- batch_5/batch_5_snake_case_converter.py
+++ batch_5/batch_5_snake_case_converter.py
@@ -1,1 +1,9 @@
 #Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+def convert_to_snake_case():
+    full_name = input("Please enter your full name: ")
+    snake_case_name = full_name.lower().replace(" ", "_")
+
+    print(snake_case_name)
+
+convert_to_snake_case()

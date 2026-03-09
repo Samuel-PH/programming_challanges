b6a88523710e65c7af09d128f8a59a371c12156d
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 21:06:21 +0800

    Custom remove prefix function using def function (DONE)

---
+++ batch_6/batch_6_custom_remove_prefix.py
@@ -0,0 +1,16 @@
+# removeprefix() remove the characters at the beginning of the string that matches the function parameter. 
+# Create a program that do the same functionality without using removeprefix() function.
+
+def remove_custom_prefix():
+    main_text = input("Please enter the main text: ")
+    prefix = input("Please enter the prefix to remove: ")
+
+    prefix_length = len(prefix)
+    if main_text[:prefix_length] == prefix:
+        result = main_text[prefix_length:]
+    else:
+        result = main_text
+        
+    print(f"Result: {result}")
+
+remove_custom_prefix()

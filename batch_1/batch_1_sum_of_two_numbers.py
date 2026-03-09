d2fbd3d31ec53b9c990e37d6565b1fcdd04266a8
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 14:47:58 +0800

    batch_1_sum_of_two_numbers is done

---
+++ batch_1/batch_1_sum_of_two_numbers.py
@@ -0,0 +1,15 @@
+#Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+def print_sum_of_two_numbers():
+    try:
+        first_input_number = float(input(f"Please enter the first number: "))
+        second_input_number = float(input(f"Please enter the second number: "))
+
+        sum_of_numbers = first_input_number + second_input_number
+
+        print(f"the sum of the two numbers you have given is {sum_of_numbers}")
+
+    except ValueError:
+        print("The number you have entered is invalid")
+
+print_sum_of_two_numbers()

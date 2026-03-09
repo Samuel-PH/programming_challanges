6a30323f2050d92971a21f8fde86dbe94fd6349a
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 20:51:16 +0800

    Made Custom lstrip function (start of the batch 6) DONE

---
+++ batch_6/batch_6_custom_lstrip.py
@@ -0,0 +1,14 @@
+#lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+def remove_leading_spaces_manual():
+    user_input = input("Please enter your text: ")
+    start_index = 0
+
+    while start_index < len(user_input) and user_input[start_index] == " ":
+        start_index = start_index + 1
+        
+    clean_text = user_input[start_index:]
+
+    print(f"Cleaned output: [{clean_text}]")
+
+remove_leading_spaces_manual()

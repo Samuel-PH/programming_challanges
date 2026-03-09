31dc45f7399f8de72d210ef97e665995c02ce0af
Samuel <samueldavetoledo678@gmail.com>
2026-03-09 15:06:12 +0800

    batch_1_first_number_powered_by_second is done

---
+++ batch_1/batch_1_first_number_powered_by_second.py
@@ -0,0 +1,15 @@
+#Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+def power_of_two_numbers():
+    try:
+        first_input_number = float(input(f"Please enter the first number: "))
+        second_input_number = float(input(f"Please enter the second number: "))
+
+        calculated_result = first_input_number ** second_input_number
+
+        print(f"The result of {first_input_number} raised to the power of {second_input_number} is: {calculated_result}")
+
+    except ValueError:
+        print("The number you have entered is invalid")
+
+power_of_two_numbers()

1268d4837e032ed6d06f1b3c3a124dd671f195bd
Samuel <samueldavetoledo678@gmail.com>
2026-03-18 15:25:28 +0800

    optimized capital

--- batch_6/batch_6_custom_capitalize.py
+++ batch_6/batch_6_custom_capitalize.py
@@ -15,3 +15,18 @@ def manual_capitalize():
     print(f"Result: {capitalized_text}")
 
 manual_capitalize()
+
+#Optimized
+
+def manual_capitalize():
+    text = input("Please enter your text: ")
+    print(f"Result: {text[:1].upper() + text[1:].lower()}")
+
+manual_capitalize()
+
+#STEPS:
+#1. Take input from the user.
+#2. Check if the input is empty. If it is, print "Result: " and return.
+#3. Get the first character of the input and convert it to uppercase.  
+#4. Get the rest of the text (from the second character to the end) and convert it to lowercase.
+#5. Add both parts together and print the result.
